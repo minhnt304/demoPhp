ac0c87d9bd673a669279d5a163e9c68a35554cf1
minhnt304 <ngotheminhdanang@gmail.com>
2019-05-22 10:27:23 +0700

    test

--- myapp/config/app.php
+++ myapp/config/app.php
@@ -174,7 +174,10 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        //updated 21.5
+        //
+        Collective\Html\HtmlServiceProvider::class,
+        
     ],
 
     /*
@@ -225,6 +228,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        //updated 21.5
+        //
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
